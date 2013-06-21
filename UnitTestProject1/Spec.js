4e0a646f99b362724aa52ed04312f4b696e10a43
Lars Markussen <lars.markussen@allegade55.dk>
2013-06-21 13:33:47 +0200

    x

---
+++ UnitTestProject1/Spec.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="jquery-1.8.0.min.js" />
+/// <reference path="xml2json.js" />
+/// <reference path="jasmine.js" />
+
+describe("XML load test", function ()
+{
+  var xml = null;
+
+  beforeEach(function ()
+  {
+    $.ajax({
+      url: "XMLFile1.xml",
+      async: false,
+      success: function (data)
+      {
+        xml = data;
+      }
+    });
+  });
+
+  it("xml is loaded", function(){
+    expect(xml.length).toBeGreaterThan(0);
+  });
+});
